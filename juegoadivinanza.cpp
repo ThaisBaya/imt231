1c7774301dcc62c0a279790cb5b3c038e77d99ee
ThaisBaya <thais.baya@ucb.edu.bo>
2024-09-17 10:15:00 -0400

    024-09-17_ 02 segunda version incluye la variable numero_secreto

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+using namespace std;
 int main(){
-std::cout<<"***************************************";
-std::cout<<"*Bienvenido al Juego de la Adivinanza!*";
-std::cout<<"***************************************";
+cout<<"***************************************"<<endl;
+cout<<"*Bienvenido al Juego de la Adivinanza!*"<<endl;
+cout<<"***************************************"<<endl;
+
+int numero_secreto =42;
+cout<<"El numero secreto es ... "<< numero_secreto <<" No lo diga a nadie!"<<endl;
+
 }
 
