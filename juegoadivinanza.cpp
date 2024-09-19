2f6033cbbeeeb527c9cee0932b3b61c448f5f1ee
ThaisBaya <thais.baya@ucb.edu.bo>
2024-09-19 11:24:27 -0400

    2024-09-19_01 controla si es=,>,<

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -6,7 +6,19 @@ cout<<"*Bienvenido al Juego de la Adivinanza!*"<<endl;
 cout<<"***************************************"<<endl;
 
 int numero_secreto =42;
-cout<<"El numero secreto es ... "<< numero_secreto <<" No lo diga a nadie!"<<endl;
+int adivina;
+cout<<"Cual es el numero?";
+cin>>adivina;
+cout<<"El valor de su numero es: "<<adivina<<endl;
+if (adivina==numero_secreto){
+     cout<<"Felicitaciones!!! ... adivino el numero secreto"<<endl;
+     }
+else if(adivina>numero_secreto){
+    cout<<"El numero ingresado es mayor que el numero secreto"<<endl;
+    }
+else{
+    cout<<"El numero ingresado es menor que el numero secreto"<<endl;
+    }
 
 }
 
