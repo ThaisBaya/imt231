27902c7eef3e8352cf01c712cd42f8bf2dc6f72d
ThaisBaya <thais.baya@ucb.edu.bo>
2024-09-24 10:24:02 -0400

    2024-09-24_02 muestra puntaje del participante

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -8,10 +8,15 @@ cout<<"***************************************"<<endl;
 const int NUMERO_SECRETO = 42;
 int adivina;
 bool no_acerto = true;
+int intentos=0;
+double puntos = 1000.0; //...guarda los puntos ganados
 while (no_acerto){
- 
+    intentos ++;
+    cout<< "Tentativa " << intentos<< endl; 
     cout<<"Cual es el numero?";
     cin>>adivina;
+    double puntos_perdidos = abs(adivina - NUMERO_SECRETO)/2.0;
+    puntos = puntos - puntos_perdidos;
     cout<<"El valor de su numero es: "<<adivina<<endl;
     bool acerto = adivina == NUMERO_SECRETO;
     bool mayor = adivina > NUMERO_SECRETO;
@@ -28,5 +33,9 @@ while (no_acerto){
 
     }
 cout << "Finalizo el juego"<< endl;
+cout<<"Adivino el numero secreto en: "<< intentos << " intentos"<<endl;
+cout.precision(2);
+cout<<fixed;
+cout<< "Su puntuacion fue de "<<puntos<<" puntos"<<endl;
 }
 
